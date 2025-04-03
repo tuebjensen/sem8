ffddcf640abfdf42a79b001b690264c3cbdf2e75
Tue Jensen <tuebjensen@gmail.com>
2025-04-03 14:38:55 +0200

    add simulation environment and test file for the environment

---
+++ ws/Sem8Env.py
@@ -0,0 +1,173 @@
+from typing import Any, override
+import gymnasium as gym
+import numpy as np
+from gymnasium import spaces
+from PIL import Image
+import pygame
+
+
+class Sem8Env(gym.Env):
+    metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 10}
+
+    def __init__(self, render_mode=None, **kwargs) -> None:
+        super().__init__()
+        self._agent_position = np.array([-1, -1], dtype=np.float32)
+        self._agent_angle = 0.0
+        self._agent_speed = 5
+        self._agent_turn_speed = 0.5
+        self._agent_radius = 30
+
+        self._target_radius = 30
+
+        self.action_space = spaces.Discrete(3)  # Forward, Left, Right
+        self.observation_space = spaces.Tuple(
+            (
+                spaces.Discrete(1),  # x coordinate
+                spaces.Discrete(1),  # y coordinate
+                spaces.Discrete(1),  # angle
+            )
+        )
+        assert render_mode is None or render_mode in self.metadata["render_modes"]
+        self.render_mode = render_mode
+
+        self.window = None
+        self.clock = None
+
+    def _get_obs(self):
+        return (
+            self._agent_position[0],
+            self._agent_position[1],
+            self._agent_angle,
+        )
+
+    def _get_info(self):
+        return {
+            "image": self._image,
+        }
+
+    @override
+    def reset(self, *, seed: int | None = None, options: dict[str, Any] | None = None):
+        super().reset(seed=seed, options=options)
+        image_path = "images/background.png"
+        self._image = pygame.image.load(image_path)
+        self._width, self._height = self._image.get_width(), self._image.get_height()
+        # im_frame = Image.open(image_path)
+        # self._image = np.array(im_frame.getdata())
+        # self._width, self._height = self._image.shape
+        self.observation_space = spaces.Tuple(
+            (
+                spaces.Discrete(self._width),  # x coordinate
+                spaces.Discrete(self._height),  # y coordinate
+                spaces.Discrete(360),  # angle
+            )
+        )
+        x_mask = np.ones(self._width, dtype=np.int8)
+        y_mask = np.ones(self._height, dtype=np.int8)
+        angle_mask = np.ones(360, dtype=np.int8)
+        x_mask[0 : self._agent_radius + 1] = 0
+        x_mask[self._agent_radius :] = 0
+        y_mask[0 : self._agent_radius + 1] = 0
+        y_mask[self._agent_radius :] = 0
+        agent_init = self.observation_space.sample(mask=(x_mask, y_mask, angle_mask))
+        self._agent_position[0], self._agent_position[1], self._agent_angle = agent_init
+
+        x_mask = np.ones(self._width, dtype=np.int8)
+        y_mask = np.ones(self._height, dtype=np.int8)
+        x_mask[0 : self._target_radius + 1] = 0
+        x_mask[self._target_radius :] = 0
+        y_mask[0 : self._target_radius + 1] = 0
+        y_mask[self._target_radius :] = 0
+        target_init = self.observation_space.sample(mask=(x_mask, y_mask, angle_mask))
+        self._target_position = np.array([target_init[0], target_init[1]])
+
+        if self.render_mode == "human":
+            self._render_frame()
+
+        observation = self._get_obs()
+        return_info = self._get_info()
+
+        return observation, return_info
+
+    def _agent_collide_with_object(self, agent_pos: np.ndarray, object_pos: np.ndarray):
+        distance = np.linalg.norm(agent_pos - object_pos)
+        return distance <= self._target_radius + self._agent_radius
+
+    @override
+    def step(self, action):
+        if action == 0:
+            # Go forward
+            self._agent_position[0] += self._agent_speed * np.cos(
+                np.radians(self._agent_angle)
+            )
+            self._agent_position[1] += self._agent_speed * np.sin(
+                np.radians(self._agent_angle)
+            )
+            self._agent_position[0:2] = np.clip(
+                self._agent_position[0:2],
+                [self._agent_radius, self._agent_radius],
+                [self._width - self._agent_radius, self._height - self._agent_radius],
+            )
+        if action == 1:
+            self._agent_angle += self._agent_turn_speed
+        if action == 2:
+            self._agent_angle -= self._agent_turn_speed
+
+        if self.render_mode == "human":
+            self._render_frame()
+
+        terminated = self._agent_collide_with_object(
+            self._agent_position, self._target_position
+        )
+        truncated = False
+        reward = 1 if terminated else -1
+        observation = self._get_obs()
+        return_info = self._get_info()
+
+        return observation, reward, terminated, truncated, return_info
+
+    @override
+    def render(self):
+        if self.render_mode == "rgb_array":
+            self._render_frame()
+
+    def _render_frame(self):
+        if self.window is None and self.render_mode == "human":
+            pygame.init()
+            pygame.display.init()
+            self.window = pygame.display.set_mode((self._width, self._height))
+        if self.clock == None and self.render_mode == "human":
+            self.clock = pygame.time.Clock()
+
+        render_surface = self._image.copy()
+        pygame.draw.circle(
+            render_surface,
+            (0, 255, 0),
+            self._agent_position.tolist(),
+            self._agent_radius,
+        )
+        pygame.draw.circle(
+            render_surface,
+            (0, 0, 255),
+            self._target_position.tolist(),
+            self._target_radius,
+        )
+
+        if self.render_mode == "human":
+            assert self.window is not None
+            assert self.clock is not None
+            self.window.blit(render_surface, render_surface.get_rect())
+            pygame.event.pump()
+            pygame.display.update()
+            self.clock.tick(self.metadata["render_fps"])
+        else:
+            return np.transpose(
+                np.array(pygame.surfarray.pixels3d(render_surface)), axes=(1, 0, 2)
+            )
+
+        def close(self):
+            if self.window is not None:
+                pygame.display.quit()
+                pygame.quit()
+
+
+gym.register(id="Sem8-v0", entry_point=Sem8Env)
--- ws/test.py
+++ ws/test.py
@@ -1,82 +1,16 @@
-from typing import final
-import pygame
-import math
+import gymnasium as gym
+import Sem8Env as _
 
-_ = pygame.init()
+env = gym.make("Sem8-v0", render_mode="human")
+observation, info = env.reset()
 
-WIDTH, HEIGHT = 800, 600
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Differential Drive Robot Simulation")
+episode_over = False
+while not episode_over:
+    action = (
+        env.action_space.sample()
+    )  # agent policy that uses the observation and info
+    observation, reward, terminated, truncated, info = env.step(action)
 
-background = pygame.image.load("images/background.png")
-background = pygame.transform.scale(background, (WIDTH, HEIGHT))
+    episode_over = terminated or truncated
 
-
-@final
-class Robot(pygame.sprite.Sprite):
-    def __init__(
-        self,
-        x: int,
-        y: int,
-        width=20,
-        height=20,
-        speed=5,
-        turn_speed=0.05,
-    ):
-        super().__init__()
-
-        self.width = width
-        self.height = height
-        self.speed = speed
-        self.turn_speed = turn_speed
-        self.x = x
-        self.y = y
-        self.angle = 0
-
-        self.rect = pygame.Rect(x, y, width, height)
-        self.image = pygame.Surface((width, height))
-
-    def update(self, keys):
-        if keys[pygame.K_UP]:
-            self.x += self.speed * math.cos(self.angle)
-            self.y += self.speed * math.sin(self.angle)
-        if keys[pygame.K_DOWN]:
-            self.x -= self.speed * math.cos(self.angle)
-            self.y -= self.speed * math.sin(self.angle)
-        if keys[pygame.K_LEFT]:
-            self.angle -= self.turn_speed
-        if keys[pygame.K_RIGHT]:
-            self.angle += self.turn_speed
-
-    def draw(self, surface):
-        self.rect.center = (math.floor(self.x), math.floor(self.y))
-        self.image.fill((0, 0, 255))
-        rotated_image = pygame.transform.rotate(self.image, -math.degrees(self.angle))
-        rotated_rect = rotated_image.get_rect(center=self.rect.center)
-        surface.blit(rotated_image, rotated_rect.topleft)
-
-
-clock = pygame.time.Clock()
-running = True
-
-robot = Robot(WIDTH // 2, HEIGHT // 2)
-
-
-while running:
-    screen.fill((255, 255, 255))
-    screen.blit(background, (0, 0))
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    keys = pygame.key.get_pressed()
-
-    robot.update(keys)
-    robot.draw(screen)
-
-    pygame.display.flip()
-
-    clock.tick(60)
-
-pygame.quit()
+env.close()
